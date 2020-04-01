7a92cac8f90f9b63bb0bca68c4e0722888d01eb0
Gleb Sapegin <glebsapegin@mail.ru>
2020-04-01 11:51:25 +0300

    Added css to OurFeatures module, corrected classNames in OurFeatures.jsx

--- donjon/src/Page Structure/Pages content/main_page/Our Features/OurFeatures.jsx
+++ donjon/src/Page Structure/Pages content/main_page/Our Features/OurFeatures.jsx
@@ -3,46 +3,45 @@ import classes from "./OurFeatures.module.css";
 
 const OurFeatures = () => {
   return (
-      <section className="our-features-block">
-        <section className="our-features-container">
-          <div className="first-first our-features-item">
-            <div className='our-features-icon'></div>
+      <section className={classes.our_features_block}>
+        <section className={classes.our_features_container}>
+          <div className={classes.first_first + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Экология</div>
           </div>
-          <div className="first-second our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.first_second + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Вид</div>
           </div>
-          <div className="first-third our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.first_third + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Средневековая атмосфера</div>
           </div>
-          <div className="second-first our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.second_first + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Соотношение цены и качества</div>
           </div>
-          <div className="second-second our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.second_second + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Расположение</div>
           </div>
-          <div className="second-third our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.second_third + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Персонал</div>
           </div>
-          <div className="third-first our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.third_first + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Эксклюзивные блюда</div>
           </div>
-          <div className="third-second our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.third_second + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Бесшовный Wi-Fi</div>
           </div>
-          <div className="third-third our-features-item">
-            <div className='our-features-icon'></div>
+          <div className={classes.third_third + ' ' + classes.our_features_item}>
+            <div className={classes.our_features_icon}></div>
             <div>Комплекс услуг в одном месте</div>
           </div>
         </section>
-
       </section>
   );
 };

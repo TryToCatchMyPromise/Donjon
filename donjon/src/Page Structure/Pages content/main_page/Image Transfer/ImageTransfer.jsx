7139ce254b52cc35f8a4de5270250982a86ce25a
Gleb Sapegin <glebsapegin@mail.ru>
2020-04-25 00:17:43 +0300

    Almost finished ImageTransfer.module.css for different devices. Have to redone width of text_overlay and text and @media queries.

--- donjon/src/Page Structure/Pages content/main_page/Image Transfer/ImageTransfer.jsx
+++ donjon/src/Page Structure/Pages content/main_page/Image Transfer/ImageTransfer.jsx
@@ -5,42 +5,64 @@ console.log(classes);
 
 const ImageTransfer = () => {
   return (
-      <div className={classes.image_flex_container}>
+      <div>
+        <div className={classes.image_flex_container}>
 
-        <div className={classes.image_items + ' ' + classes.secondImage}>
-          <div className={classes.overlay}></div>
-          <div className={classes.data_container}>
-            <div className={classes.name}>Гостиница</div>
-            <div className={classes.monogram}></div>
-            <div className={classes.text}>Очень длинный текст про гостиницу и то,<br/> как там круто жить</div>
-            <div className={classes.phone}>+7 (4842) 56-57-56</div>
-            <div className={classes.new_btn}>Подробнее</div>
-          </div>
-        </div>
-
-        <div className={classes.image_items + ' ' + classes.firstImage}>
-          <div className={classes.overlay}></div>
-          <div className={classes.data_container}>
-            <div className={classes.name}>Ресторан</div>
-            <div className={classes.monogram}></div>
-            <div className={classes.text}>Очень длинный текст про ресторан и то,<br/> как там круто есть, пить, танцевать.<br/> Всем советуем!</div>
-            <div className={classes.phone}>+7 (4842) 22-49-96</div>
-            <div className={classes.new_btn}>Подробнее</div>
+          <div className={classes.image_items + ' ' + classes.secondImage}>
+            <div className={classes.overlay}></div>
+            <div className={classes.data_container}>
+              <div className={classes.name}>Гостиница</div>
+              <div className={classes.monogram}></div>
+              <div className={classes.text_overlay}>Ваши кони устали, и сил уже нет?
+                Гостиница на 13 номеров послужит отличным пристанищем для путников со всех уголков света
+              </div>
+              <div className={classes.phone}>+7 (4842) 56-57-56</div>
+              <div className={classes.button}>Подробнее</div>
+            </div>
+            <div className={classes.text}>Ваши кони устали, и сил уже нет?
+              Гостиница на 13 номеров послужит отличным пристанищем для путников со всех уголков света
+            </div>
           </div>
 
-        </div>
-        <div className={classes.image_items + ' ' + classes.thirdImage}>
-          <div className={classes.overlay}></div>
-          <div className={classes.data_container}>
-            <div className={classes.name}>Услуги</div>
-            <div className={classes.monogram}></div>
-            <div className={classes.text}>Очень длинный текст про услуги и то, как<br/> там круто отдыхать</div>
-            <div className={classes.phone}>+7 (4842) 56-57-56</div>
-            <div className={classes.new_btn}>Подробнее</div>
+          <div className={classes.image_items + ' ' + classes.firstImage}>
+            <div className={classes.overlay}></div>
+            <div className={classes.data_container}>
+              <div className={classes.name}>Ресторан</div>
+              <div className={classes.monogram}></div>
+              <div className={classes.text_overlay}>Вы голодны, а доставка слона из
+                Индии вам не по карману? Искушать яств местных и заморских
+                смогут позволить себе люди из всех сословий любого достатка в залах
+                нашего ресторана
+              </div>
+              <div className={classes.phone}>+7 (4842) 22-49-96</div>
+              <div className={classes.button}>Подробнее</div>
+            </div>
+            <div className={classes.text}>Вы голодны, а доставка слона из
+              Индии вам не по карману? Искушать яств местных и заморских
+              смогут позволить себе люди из всех сословий любого достатка в залах
+              нашего ресторана
+            </div>
           </div>
 
+          <div className={classes.image_items + ' ' + classes.thirdImage}>
+            <div className={classes.overlay}></div>
+            <div className={classes.data_container}>
+              <div className={classes.name}>Услуги</div>
+              <div className={classes.monogram}></div>
+              <div className={classes.text_overlay}>Совершить омовения в бассейне, закалить тела горячим паром в сауне
+                и развить боевые навыки в тренажёрном зале
+                сможет дружная компания из любого ордена!</div>
+              <div className={classes.phone}>+7 (4842) 56-57-56</div>
+              <div className={classes.button}>Подробнее</div>
+            </div>
+            <div className={classes.text}>Совершить омовения в бассейне, закалить тела горячим паром в сауне
+              и развить боевые навыки в тренажёрном зале
+              сможет дружная компания из любого ордена!
+            </div>
+          </div>
         </div>
       </div>
+
   );
 };
 

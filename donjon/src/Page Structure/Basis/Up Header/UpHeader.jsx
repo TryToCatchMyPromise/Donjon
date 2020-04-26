79283daa5d19e7df25ff79872847e8bf2c33b8b5
Gleb Sapegin <glebsapegin@mail.ru>
2020-04-26 05:05:43 +0300

    Worked with UpHeader.module.css, done adoptive design up to 900 px.

--- donjon/src/Page Structure/Basis/Up Header/UpHeader.jsx
+++ donjon/src/Page Structure/Basis/Up Header/UpHeader.jsx
@@ -5,7 +5,9 @@ import Navigation from "./Navigation/Navigation";
 const UpHeader = () => {
   return (
       <div className={classes.header}>
-        <div className={classes.logo}></div>
+        <div className={classes.logo_container}>
+          <div className={classes.logo}></div>
+        </div>
         <Navigation/>
         <div className={classes.btn_container}>
           <div className={classes.btn}>Оставить заявку</div>
